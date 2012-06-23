805fef9dab2cf1f9a4cf3f2da441cabd1923893b
Gabriel Pickard <gpickard@uos.de>
2012-06-23 18:37:04 +0200

    Core Message Passing

--- src/garbaje_compiler/core.clj
+++ src/garbaje_compiler/core.clj
@@ -1,1 +1,14 @@
 (ns garbaje-compiler.core)
+
+(defprotocol Receiver
+  (tell [this message sender]))
+
+(defn send-msg
+  [sender message recipient]
+  (send recipient tell message sender))
+
+(deftype Actor [state behavior]
+  Receiver
+  (tell [this message sender]
+    (let [[state1 behavior1] (behavior state message)]
+      (Actor. state1 behavior1))))
